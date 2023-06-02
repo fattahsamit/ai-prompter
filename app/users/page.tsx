e1bb58129fa46bd21e93aedada0a5e235e0b9016
fattahsamit <fattahsamit@gmail.com>
2023-06-02 16:31:06 +0600

    SEO and metadata

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,16 @@
 // http://localhost:3000/user
 
+// static metadata
+export const metadata = {
+  title: "User",
+};
+
+// Dynamic metadata
+export async function generateMetadata({ params, searchParams }: any) {
+  const product = await getProduct(params.id);
+  return { title: product.title };
+}
+
 const page = () => {
   return (
     <div>
