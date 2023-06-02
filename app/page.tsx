a54a4343c3d1677a6629e3b280b285414a3e4e60
fattahsamit <fattahsamit@gmail.com>
2023-06-02 21:58:30 +0600

    home page banner texts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Explore & share <br className="max-md:hidden" />
+        <span className="orange_gradient">AI powered prompts</span>
+      </h1>
+      <p className="desc text-center">
+        AI prompter Lorem ipsum dolor sit amet consectetur adipis elit.
+        Excepturi enim, neque earum. Sapiente atque laborum iusto laboriosam.
+      </p>
+
+      {/* Feed */}
+    </section>
+  );
 };
 
 export default Home;
