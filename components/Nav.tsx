0ffb219ab70978ea70550ebdccbb3576d1e3f382
fattahsamit <fattahsamit@gmail.com>
2023-06-02 22:11:14 +0600

    importing necessary packages and displaying nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,3 +1,8 @@
+import Link from "next/link";
+import Image from "next/image";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { useState, useEffect } from "react";
+
 const Nav = () => {
   return <div>Nav</div>;
 };
