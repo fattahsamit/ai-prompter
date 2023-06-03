496f807b05dc650e716c48b1a07c2967e89f9eb4
fattahsamit <fattahsamit@gmail.com>
2023-06-03 12:57:01 +0600

    provider, database

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -5,12 +5,16 @@ import { signIn } from "next-auth/react";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   async session({ session }) {},
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      // serverless -> Lambda -> dynamodb
+    } catch (error) {}
+  },
 });
 
 export { handler as GET, handler as POST };
