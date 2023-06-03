54112e3ae63082743e1e538dd9a0e04d41bce469
fattahsamit <fattahsamit@gmail.com>
2023-06-03 13:44:56 +0600

    mongo signin

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
@@ -12,8 +14,23 @@ const handler = NextAuth({
   async session({ session }) {},
   async signIn({ profile }) {
     try {
-      // serverless -> Lambda -> dynamodb
-    } catch (error) {}
+      await connectToDB();
+      //   Check if a user already exists
+      const userExists = await User.findOne({ email: profile.email });
+
+      // if not create a new user and save it to a database
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 
